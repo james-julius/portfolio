deb894c16e969a63fc3ce4ece1b7b0550437f3cf
James <jamesjulius@Jamess-MacBook-Pro.local>
2020-01-25 20:18:54 +0000

    Just before wave animation

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,6 +7,13 @@ function Home() {
         <div id="home">
             <div id="islands">
                 <div id="bay">
+                    {/* <svg>
+                        <filter id="bay-waves" x="0" y="0" width="100%" height="100%">
+                            <feTurbulence id="sea-filter" numOctaves="3" seed="2" baseFrequency="0.02 0.05"></feTurbulence>
+                            <feDisplacementMap scale="20" in="SourceGraphic"></feDisplacementMap>
+                            <animate xlinkHref="#sea-filter" attributeName="baseFrequency" dur="60s" keyTimes="0;0.5;1" values="0.02 0.06;0.04 0.08;0.02 0.06" repeatCount="indefinite"/>
+                          </filter>
+                    </svg> */}
                     <div id="waterfall">
                         <div id="content-background">
                             <h1>James Darby</h1>
