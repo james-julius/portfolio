e58cd4af51115b735a8a5803f0048ff61fd866fd
James <jamesjulius@Jamess-MacBook-Pro.local>
2020-01-26 17:47:56 +0000

    SVG backgrounds finally fixed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import styled, {keyframes} from 'styled-components';
 import './Home.css';
 
 function Home() {
+    const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
     return (
         <div id="home">
             <div id="content-background">
@@ -11,23 +12,23 @@ function Home() {
             </div>
             <div id="islands">
                 <div id="bay">
-                    <svg>
+                    { (isChrome) ? <svg>
                         <filter id="bay-waves" x="0" y="0" width="100%" height="100%">
                             <feTurbulence id="sea-filter" numOctaves="3" seed="2" baseFrequency="0.02 0.05"></feTurbulence>
                             <feDisplacementMap scale="20" in="SourceGraphic"></feDisplacementMap>
                             <animate xlinkHref="#sea-filter" attributeName="baseFrequency" dur="60s" 
                             keyTimes="0;0.5;1" values="0.02 0.06;0.04 0.08;0.02 0.06" repeatCount="indefinite"/>
                           </filter>
-                    </svg>
+                    </svg>: null}
                     <div id="waterfall">
-                          <svg>
+                          { (isChrome)? <svg>
                             <filter id="waterfall" x="0" y="0" width="100%" height="100%">
                                 <feTurbulence id="waves-filter" numOctaves="6" seed="2" baseFrequency="0.05 0.90"></feTurbulence>
                                 <feDisplacementMap scale="20" in="SourceGraphic"></feDisplacementMap>
                                 <animate xlinkHref="#waves-filter" attributeName="baseFrequency" dur="60s"
                                 keyTimes="0;0.5;1" values="0.02 0.2; 0.6 0.08; 0.02 0.06" repeatCount="indefinite"/>
                             </filter>
-                        </svg>
+                          </svg> : null}
                     </div>
                 </div>
             </div>
